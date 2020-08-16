97e558e6d116cab93e73a3ea9e2f10402bb9633e
Mohammad Umar Khan <khanumar620@gmail.com>
2020-08-16 00:18:03 +0000

    Task Completed

--- network.tf
+++ network.tf
@@ -1,12 +1,3 @@
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "Umar"
-  }
-}
-
 resource "aws_internet_gateway" "gw" {
   vpc_id = "${aws_vpc.main.id}"
 
